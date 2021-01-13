b734b3a29c54a8593785a407febea46515820b6f
ratkosrb <ratkomladic2@abv.bg>
2021-01-14 01:53:47 +0200

    1.12.0.5496 PTR

--- Global.ChatTypes.cs
+++ Global.ChatTypes.cs
@@ -1,37 +1,38 @@
 // Auto generated file
-// Patch: 1.11.2
-// Build: 5464
+// Patch: 0.12.0
+// Build: 5496
 
 Public Enum ChatMsg
 {
-    CHAT_MSG_SAY = 0x0,                                             // 0x000
-    CHAT_MSG_PARTY = 0x1,                                           // 0x001
-    CHAT_MSG_RAID = 0x2,                                            // 0x002
-    CHAT_MSG_GUILD = 0x3,                                           // 0x003
-    CHAT_MSG_OFFICER = 0x4,                                         // 0x004
-    CHAT_MSG_YELL = 0x5,                                            // 0x005
-    CHAT_MSG_WHISPER = 0x6,                                         // 0x006
-    CHAT_MSG_WHISPER_INFORM = 0x7,                                  // 0x007
-    CHAT_MSG_EMOTE = 0x8,                                           // 0x008
-    CHAT_MSG_TEXT_EMOTE = 0x9,                                      // 0x009
-    CHAT_MSG_SYSTEM = 0xA,                                          // 0x00A
-    CHAT_MSG_MONSTER_SAY = 0xB,                                     // 0x00B
-    CHAT_MSG_MONSTER_YELL = 0xC,                                    // 0x00C
-    CHAT_MSG_MONSTER_WHISPER = 0xD,                                 // 0x00D
-    CHAT_MSG_MONSTER_EMOTE = 0xE,                                   // 0x00E
-    CHAT_MSG_CHANNEL = 0xF,                                         // 0x00F
-    CHAT_MSG_CHANNEL_JOIN = 0x10,                                   // 0x010
-    CHAT_MSG_CHANNEL_LEAVE = 0x11,                                  // 0x011
-    CHAT_MSG_CHANNEL_LIST = 0x12,                                   // 0x012
-    CHAT_MSG_CHANNEL_NOTICE = 0x13,                                 // 0x013
-    CHAT_MSG_CHANNEL_NOTICE_USER = 0x14,                            // 0x014
-    CHAT_MSG_AFK = 0x15,                                            // 0x015
-    CHAT_MSG_DND = 0x16,                                            // 0x016
-    CHAT_MSG_COMBAT_LOG = 0x17,                                     // 0x017
-    CHAT_MSG_IGNORED = 0x18,                                        // 0x018
-    CHAT_MSG_SKILL = 0x19,                                          // 0x019
-    CHAT_MSG_LOOT = 0x1A,                                           // 0x01A
-    CHAT_MSG_MONEY = 0x1B,                                          // 0x01B
-    CHAT_MSG_RAID_LEADER = 0x1C,                                    // 0x01C
-    CHAT_MSG_RAID_WARNING = 0x1D,                                   // 0x01D
+    CHAT_MSG_ADDON = 0x0,                                           // 0x000
+    CHAT_MSG_SAY = 0x1,                                             // 0x001
+    CHAT_MSG_PARTY = 0x2,                                           // 0x002
+    CHAT_MSG_RAID = 0x3,                                            // 0x003
+    CHAT_MSG_GUILD = 0x4,                                           // 0x004
+    CHAT_MSG_OFFICER = 0x5,                                         // 0x005
+    CHAT_MSG_YELL = 0x6,                                            // 0x006
+    CHAT_MSG_WHISPER = 0x7,                                         // 0x007
+    CHAT_MSG_WHISPER_INFORM = 0x8,                                  // 0x008
+    CHAT_MSG_EMOTE = 0x9,                                           // 0x009
+    CHAT_MSG_TEXT_EMOTE = 0xA,                                      // 0x00A
+    CHAT_MSG_SYSTEM = 0xB,                                          // 0x00B
+    CHAT_MSG_MONSTER_SAY = 0xC,                                     // 0x00C
+    CHAT_MSG_MONSTER_YELL = 0xD,                                    // 0x00D
+    CHAT_MSG_MONSTER_WHISPER = 0xE,                                 // 0x00E
+    CHAT_MSG_MONSTER_EMOTE = 0xF,                                   // 0x00F
+    CHAT_MSG_CHANNEL = 0x10,                                        // 0x010
+    CHAT_MSG_CHANNEL_JOIN = 0x11,                                   // 0x011
+    CHAT_MSG_CHANNEL_LEAVE = 0x12,                                  // 0x012
+    CHAT_MSG_CHANNEL_LIST = 0x13,                                   // 0x013
+    CHAT_MSG_CHANNEL_NOTICE = 0x14,                                 // 0x014
+    CHAT_MSG_CHANNEL_NOTICE_USER = 0x15,                            // 0x015
+    CHAT_MSG_AFK = 0x16,                                            // 0x016
+    CHAT_MSG_DND = 0x17,                                            // 0x017
+    CHAT_MSG_COMBAT_LOG = 0x18,                                     // 0x018
+    CHAT_MSG_IGNORED = 0x19,                                        // 0x019
+    CHAT_MSG_SKILL = 0x1A,                                          // 0x01A
+    CHAT_MSG_LOOT = 0x1B,                                           // 0x01B
+    CHAT_MSG_MONEY = 0x1C,                                          // 0x01C
+    CHAT_MSG_RAID_LEADER = 0x1D,                                    // 0x01D
+    CHAT_MSG_RAID_WARNING = 0x1E,                                   // 0x01E
 }
