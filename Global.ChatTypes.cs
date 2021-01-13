5c8b8839189ef373aa67c1c3edfa1253c2943518
ratkosrb <ratkomladic2@abv.bg>
2021-01-14 00:50:24 +0200

    0.5.3.3368

---
+++ Global.ChatTypes.cs
@@ -0,0 +1,32 @@
+// Auto generated file
+// Patch: 0.5.3
+// Build: 3368
+
+Public Enum ChatMsg
+{
+    CHAT_MSG_SAY = 0x0,                                             // 0x000
+    CHAT_MSG_PARTY = 0x1,                                           // 0x001
+    CHAT_MSG_GUILD = 0x2,                                           // 0x002
+    CHAT_MSG_OFFICER = 0x3,                                         // 0x003
+    CHAT_MSG_YELL = 0x4,                                            // 0x004
+    CHAT_MSG_WHISPER = 0x5,                                         // 0x005
+    CHAT_MSG_WHISPER_INFORM = 0x6,                                  // 0x006
+    CHAT_MSG_EMOTE = 0x7,                                           // 0x007
+    CHAT_MSG_TEXT_EMOTE = 0x8,                                      // 0x008
+    CHAT_MSG_SYSTEM = 0x9,                                          // 0x009
+    CHAT_MSG_MONSTER_SAY = 0xA,                                     // 0x00A
+    CHAT_MSG_MONSTER_YELL = 0xB,                                    // 0x00B
+    CHAT_MSG_MONSTER_EMOTE = 0xC,                                   // 0x00C
+    CHAT_MSG_CHANNEL = 0xD,                                         // 0x00D
+    CHAT_MSG_CHANNEL_JOIN = 0xE,                                    // 0x00E
+    CHAT_MSG_CHANNEL_LEAVE = 0xF,                                   // 0x00F
+    CHAT_MSG_CHANNEL_LIST = 0x10,                                   // 0x010
+    CHAT_MSG_CHANNEL_NOTICE = 0x11,                                 // 0x011
+    CHAT_MSG_CHANNEL_NOTICE_USER = 0x12,                            // 0x012
+    CHAT_MSG_AFK = 0x13,                                            // 0x013
+    CHAT_MSG_DND = 0x14,                                            // 0x014
+    CHAT_MSG_COMBAT_LOG = 0x15,                                     // 0x015
+    CHAT_MSG_IGNORED = 0x16,                                        // 0x016
+    CHAT_MSG_SKILL = 0x17,                                          // 0x017
+    CHAT_MSG_LOOT = 0x18,                                           // 0x018
+}
