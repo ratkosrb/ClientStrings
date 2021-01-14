8d5197b848b0cb7c9752f6ffa0cfd86cb2243514
ratkosrb <ratkomladic2@abv.bg>
2021-01-14 13:00:47 +0200

    2.4.0.7897 PTR

--- Global.ChatTypes.cs
+++ Global.ChatTypes.cs
@@ -1,19 +1,19 @@
 // Auto generated file
-// Patch: 2.3.3
-// Build: 7799
+// Patch: 0.4.0
+// Build: 7897
 
 Public Enum ChatMsg
 {
     CHAT_MSG_ADDON = 0x0,                                           // 0x000
-    CHAT_MSG_SAY = 0x1,                                             // 0x001
-    CHAT_MSG_GUILD = 0x2,                                           // 0x002
-    CHAT_MSG_OFFICER = 0x3,                                         // 0x003
-    CHAT_MSG_YELL = 0x4,                                            // 0x004
-    CHAT_MSG_WHISPER = 0x5,                                         // 0x005
-    CHAT_MSG_WHISPER_INFORM = 0x6,                                  // 0x006
-    CHAT_MSG_EMOTE = 0x7,                                           // 0x007
-    CHAT_MSG_TEXT_EMOTE = 0x8,                                      // 0x008
-    CHAT_MSG_SYSTEM = 0x9,                                          // 0x009
+    CHAT_MSG_SYSTEM = 0x1,                                          // 0x001
+    CHAT_MSG_SAY = 0x2,                                             // 0x002
+    CHAT_MSG_GUILD = 0x3,                                           // 0x003
+    CHAT_MSG_OFFICER = 0x4,                                         // 0x004
+    CHAT_MSG_YELL = 0x5,                                            // 0x005
+    CHAT_MSG_WHISPER = 0x6,                                         // 0x006
+    CHAT_MSG_WHISPER_INFORM = 0x7,                                  // 0x007
+    CHAT_MSG_EMOTE = 0x8,                                           // 0x008
+    CHAT_MSG_TEXT_EMOTE = 0x9,                                      // 0x009
     CHAT_MSG_MONSTER_SAY = 0xA,                                     // 0x00A
     CHAT_MSG_MONSTER_YELL = 0xB,                                    // 0x00B
     CHAT_MSG_MONSTER_WHISPER = 0xC,                                 // 0x00C
@@ -26,11 +26,24 @@ Public Enum ChatMsg
     CHAT_MSG_CHANNEL_NOTICE_USER = 0x13,                            // 0x013
     CHAT_MSG_AFK = 0x14,                                            // 0x014
     CHAT_MSG_DND = 0x15,                                            // 0x015
-    CHAT_MSG_COMBAT_LOG = 0x16,                                     // 0x016
-    CHAT_MSG_IGNORED = 0x17,                                        // 0x017
-    CHAT_MSG_SKILL = 0x18,                                          // 0x018
-    CHAT_MSG_LOOT = 0x19,                                           // 0x019
-    CHAT_MSG_MONEY = 0x1A,                                          // 0x01A
-    CHAT_MSG_RAID_LEADER = 0x1B,                                    // 0x01B
-    CHAT_MSG_RAID_WARNING = 0x1C,                                   // 0x01C
+    CHAT_MSG_IGNORED = 0x16,                                        // 0x016
+    CHAT_MSG_SKILL = 0x17,                                          // 0x017
+    CHAT_MSG_LOOT = 0x18,                                           // 0x018
+    CHAT_MSG_MONEY = 0x19,                                          // 0x019
+    CHAT_MSG_OPENING = 0x1A,                                        // 0x01A
+    CHAT_MSG_TRADESKILLS = 0x1B,                                    // 0x01B
+    CHAT_MSG_PET_INFO = 0x1C,                                       // 0x01C
+    CHAT_MSG_COMBAT_MISC_INFO = 0x1D,                               // 0x01D
+    CHAT_MSG_COMBAT_XP_GAIN = 0x1E,                                 // 0x01E
+    CHAT_MSG_COMBAT_HONOR_GAIN = 0x1F,                              // 0x01F
+    CHAT_MSG_COMBAT_FACTION_CHANGE = 0x20,                          // 0x020
+    CHAT_MSG_BG_SYSTEM_NEUTRAL = 0x21,                              // 0x021
+    CHAT_MSG_BG_SYSTEM_ALLIANCE = 0x22,                             // 0x022
+    CHAT_MSG_BG_SYSTEM_HORDE = 0x23,                                // 0x023
+    CHAT_MSG_RAID_LEADER = 0x24,                                    // 0x024
+    CHAT_MSG_RAID_WARNING = 0x25,                                   // 0x025
+    CHAT_MSG_RAID_BOSS_WHISPER = 0x26,                              // 0x026
+    CHAT_MSG_RAID_BOSS_EMOTE = 0x27,                                // 0x027
+    CHAT_MSG_FILTERED = 0x28,                                       // 0x028
+    CHAT_MSG_BATTLEGROUND_LEADER = 0x29,                            // 0x029
 }
